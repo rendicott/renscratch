e560994661f4cd5895c74ff971e3aaaff7d83090
Russell Endicott <russell.endicott@ge.com>
2021-06-16 16:46:35 +0000

    initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+target=$1
+set -x
+apk upgrade --update
+apk add bash ca-certificates
+touch haproxy.cfg
+cat << EOF > haproxy.cfg
+global
+        log stdout format raw daemon debug
+
+defaults
+        log     global
+        mode    tcp
+        option  tcplog
+        option  dontlognull
+        timeout client 500s
+        timeout connect 500s
+        timeout server 500s
+
+frontend site
+   bind *:80
+   mode tcp
+   option tcplog
+   default_backend site
+
+backend site
+server site-redirect-nonssl $target:80
+
+
+frontend site_ssl
+   bind *:443
+   mode tcp
+   option tcplog
+   default_backend site_ssl
+
+backend site_ssl
+server site-redirect-ssl $target:443
+EOF
+haproxy -f haproxy.cfg
+[root@ip-10-232-227-86 hap]#
